20f3350992dafc585fe6db2d1c7c8def95d4ca50
diogoamss <diogoamss@gmail.com>
2025-09-04 17:42:10 -0300

    w.i.p 0.1 | add model/Produto.java

--- src/main/java/com/example/api_produto/model/Produto.java
+++ src/main/java/com/example/api_produto/model/Produto.java
@@ -1,5 +1,29 @@
 package com.example.api_produto.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Double preco;
+
+    public Produto() {}
+
+    public Produto(String nome, Double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    //getters e setters
+    public long getId() { return id;}
+    public void setId(Long id) { this.id = id;}
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+    public Double getPreco() { return preco;}
+    public void getPreco(Double preco) {this.preco = preco;}
     
 }
